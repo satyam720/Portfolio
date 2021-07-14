88a05f5e5d6d562420e83629070b4b9caaf83472
satyam1906 <psatyam1618@gmail.com>
2021-07-14 23:53:56 +0530

    portfolioList added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,77 @@
+import PortfolioList from '../portfolioList/PortfolioList';
 import './portfolio.scss';
+import { useState } from 'react';
 
-export default function portfolio() {
+export default function Portfolio() {
+
+    const [selected, setSelected] = useState("featured");
+    const list = [
+        {
+          id: "featured",
+          title: "Featured",
+        },
+        {
+          id: "web",
+          title: "Web App",
+        },
+        {
+          id: "mobile",
+          title: "Mobile App",
+        },
+        {
+          id: "design",
+          title: "Design",
+        },
+        {
+          id: "content",
+          title: "Content",
+        },
+      ];
+
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+               {list.map((item) => (
+                  <PortfolioList 
+                      title={item.title}
+                      active={selected === item.id}
+                      setSelected={setSelected}
+                      id={item.id}
+                  />))}
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="https://stormotion.io/blog/content/images/2020/09/mobile-banking-app.jpg" alt=""></img>
+                    <h3>Banking App</h3>
+                    
+                </div>
+                <div className="item">
+                    <img src="https://stormotion.io/blog/content/images/2020/09/mobile-banking-app.jpg" alt=""></img>
+                    <h3>Banking App</h3>
+                    
+                </div>
+                <div className="item">
+                    <img src="https://stormotion.io/blog/content/images/2020/09/mobile-banking-app.jpg" alt=""></img>
+                    <h3>Banking App</h3>
+                    
+                </div>
+                <div className="item">
+                    <img src="https://stormotion.io/blog/content/images/2020/09/mobile-banking-app.jpg" alt=""></img>
+                    <h3>Banking App</h3>
+                    
+                </div>
+                <div className="item">
+                    <img src="https://stormotion.io/blog/content/images/2020/09/mobile-banking-app.jpg" alt=""></img>
+                    <h3>Banking App</h3>
+                    
+                </div>
+                <div className="item">
+                    <img src="https://stormotion.io/blog/content/images/2020/09/mobile-banking-app.jpg" alt=""></img>
+                    <h3>Banking App</h3>
+                    
+                </div>
+            </div>
         </div>
     )
 }
