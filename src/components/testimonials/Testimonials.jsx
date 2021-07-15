3d8bb91e5072fbeec037ffe1056f0ecfc1d90d9b
satyam1906 <psatyam1618@gmail.com>
2021-07-15 12:58:19 +0530

    testimonials added

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,28 +1,66 @@
 import './testimonials.scss';
 
 export default function Testimonials() {
+
+    const data = [
+        {
+          id: 1,
+          name: "Tom Durden",
+          title: "Senior Developer",
+          img:
+            "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+          icon: "assets/twitter.png",
+          desc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
+        },
+        {
+          id: 2,
+          name: "Alex Kalinski",
+          title: "Co-Founder of DELKA",
+          img:
+            "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+          icon: "assets/youtube.png",
+          desc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
+          featured: true,
+        },
+        {
+          id: 3,
+          name: "Martin Harold",
+          title: "CEO of ALBI",
+          img:
+            "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+          icon: "assets/linkedin.png",
+          desc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+        },
+      ];
+
     return (
         <div className='testimonials' id="testimonials">
             <h1>Testimonials</h1>
             <div className="container">
-                <div className="card">
+            {data.map((d)=>(
+                <div className={d.featured ? 'card featured': 'card'}>
                     <div className="top">
                         <img src="assets/right-arrow.png" className="left" alt=""></img>
-                        <img className="user" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8EX6li55wPbKt76wIJqfLAdreXNkvFB7paA&usqp=CAU" alt=""></img>
+                        <img className="user"
+                         src={d.img} alt=""></img>
                        
-                        <img src="assets/youtube.png" 
-                        className="right" alt=""></img>
+                        <img 
+                        src={d.icon} 
+                        className="right"
+                         alt=""></img>
                     </div>
                     <div className="center">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    {d.desc}
                      
                     </div>
                     <div className="bottom">
-                        <h3>Alex</h3>
-                        <h4>CEO of LAMA</h4>
+                        <h3>{d.name}</h3>
+                        <h4>{d.title}</h4>
                     </div>
-                </div>
+                </div>))}
             </div>
         </div>
     )
